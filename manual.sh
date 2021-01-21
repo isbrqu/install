a653243a0810b0e54d14da3268009f88793b6985
Isaac Bruno Quiroga <34814199+isbrqu@users.noreply.github.com>
2021-01-20 23:59:59 -0300

    agrega la instalacion de st

--- manual.sh
+++ manual.sh
@@ -5,7 +5,9 @@ url="https://github.com/tmux/tmux/releases/download/3.1c/tmux-3.1c.tar.gz"\
 && tar -zxf tmux-*.tar.gz\
 && cd tmux-*/\
 && ./configure\
-&& make && sudo make install
+&& make\
+&& sudo make install\
+&& cd ..
 
 # vim
 # url="https://github.com/vim/vim.git"
@@ -26,7 +28,7 @@ url="https://github.com/junegunn/fzf.git"\
 
 # googler
 url="https://raw.githubusercontent.com/jarun/googler/v4.3.1/googler"\
-&& sudo curl -o /usr/local/bin/googler "$url"
+&& sudo curl -o /usr/local/bin/googler "$url"\
 && sudo chmod +x /usr/local/bin/googler
 
 # i3status
@@ -34,10 +36,12 @@ url="https://i3wm.org/i3status/i3status-2.13.tar.bz2"\
 && wget "$url"\
 && tar -zxf i3status-*.tar.gz\
 && autoreconf -fi\
-&& mkdir build && cd build\
+&& mkdir build\
+&& cd build\
 && ../configure --disable-sanitizers\
 && make -j$(nproc)\
-&& sudo make install
+&& sudo make install\
+&& cd ..
 
 # dmenu
 #url="https://dl.suckless.org/tools/dmenu-5.0.tar.gz"\
@@ -45,5 +49,11 @@ url="https://i3wm.org/i3status/i3status-2.13.tar.bz2"\
 #&& tar -zxf dmenu-*.tar.gz
 
 # st
-# todo
+url="https://github.com/isbrqu/st/archive/master.zip"\
+&& wget "$url"\
+&& unzip -q st-master\
+&& cd st-master\
+&& sudo make clean install\
+&& cd ..
+
 
