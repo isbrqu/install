02005b11376e857d65b820eaf3f9fd18a113d13a
Isaac Bruno Quiroga <34814199+isbrqu@users.noreply.github.com>
2021-01-20 23:36:27 -0300

    reamplaza todo el sources.list

--- install.sh
+++ install.sh
@@ -4,6 +4,4 @@ username="isbrqu"
 pathdot=".config/dot"
 echo "$username  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 && vi /etc/sudoers
-cat sources.list >> /etc/apt/sources.list
-&& vi /etc/apt/sources.list
-
+cat sources.list > /etc/apt/sources.list
