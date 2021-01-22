df95860504b18c011b3fc43b8f3fc55865d3fd0a
Isaac Bruno Quiroga <isbrqu@gmail.com>
2021-01-21 22:53:04 -0300

    estructura mejor las instalaciones

--- manual.sh
+++ manual.sh
@@ -1,59 +1,115 @@
 #!/usr/bin/env bash
-# tmux
-url="https://github.com/tmux/tmux/releases/download/3.1c/tmux-3.1c.tar.gz"\
-&& wget "$url"\
-&& tar -zxf tmux-*.tar.gz\
-&& cd tmux-*/\
+
+ferror='~/scr-main/error'
+
+ztmux='tmux-3.1c.tar.gz'
+zscim='v0.7.0.tar.gz'
+zdmenu='dmenu-5.0.tar.gz'
+zi3status='i3status-2.13.tar.bz2'
+
+url_tmux="https://github.com/tmux/tmux/releases/download/3.1c/$ztmux"
+url_scim="https://github.com/andmarti1424/sc-im/archive/$zscim"
+url_dmenu="https://dl.suckless.org/tools/$zdmenu"
+url_i3status="https://i3wm.org/i3status/$zi3status"
+url_vim="https://github.com/vim/vim.git"
+url_fzf="https://github.com/junegunn/fzf.git"
+url_st="https://github.com/isbrqu/st.git"
+url_googler="https://raw.githubusercontent.com/jarun/googler/v4.3.1/googler"
+
+ftmux="${ztmux%.tar.gz}"
+fscim="sc-im-0.7.0"
+fdmenu="${zdmenu%.tar.gz}"
+fi3status="${zi3status%.tar.bz2}"
+fvim="vim"
+ffzf="fzf"
+fst="st"
+
+([[ -d z ]] || mkdir z)\
+&& cd z\
+&& mkdir log
+
+echo "tmux"
+wget "$url_tmux" --quiet
+(( $? == 0 )) || (echo "error tmux" && exit 1)
+
+echo "sc-im"
+wget "$url_scim" --quiet
+(( $? == 0 )) || (echo "error sc-im" && exit 1)
+
+echo "dmenu"
+wget "$url_dmenu" --quiet
+(( $? == 0 )) || (echo "error dmenu" && exit 1)
+
+echo "i3status"
+wget "$url_i3status" --quiet
+(( $? == 0 )) || (echo "error i3status" && exit 1)
+
+echo "vim"
+git clone --depth 1 --quiet -- "$url_vim" vim
+(( $? == 0 )) || (echo "error vim" && exit 1)
+
+echo "fzf"
+git clone --depth 1 --quiet -- "$url_fzf" fzf
+(( $? == 0 )) || (echo "error fzf" && exit 1)
+
+echo "st"
+git clone --depth 1 --quiet -- "$url_st" st
+(( $? == 0 )) || (echo "error st" && exit 1)
+
+echo "googler"
+wget "$url_googler" --quiet
+(( $? == 0 )) || (echo "error googler" && exit 1)
+
+tar --extract --gunzip --file="$ztmux"\
+&& rm "$ztmux"
+
+tar --extract --gunzip --file="$zscim"\
+&& rm "$zscim"
+
+tar --extract --gunzip --file="$zdmenu"\
+&& rm "$zdmenu"
+
+tar --extract --bzip2 --file="$zi3status"\
+&& rm "$zi3status"
+
+(cd "$ftmux"\
 && ./configure\
 && make\
 && sudo make install\
-&& cd ..
+&& cd ..)\
+|| echo "error tmux" >> "$ferror"
 
-# vim
-# url="https://github.com/vim/vim.git"
-# git clone --depth 1 "$url"
+(cd "$fscim"\
+&& sudo make -C src install\
+&& cd ..)\
+|| echo "error scim" >> "$ferror"
 
-# kakoune
-# url="https://github.com/mawww/kakoune/releases/download/v2020.09.01/kakoune-2020.09.01.tar.bz2"
-# wget "$url"
+# (cd "$fst"\
+# && sudo make clean install\
+# && cd ..)\
+# || echo "error st" >> "$ferror"
 
-# sc-im
-# url="https://github.com/andmarti1424/sc-im/archive/v0.7.0.tar.gz"
-# wget "$url"
-
-# fzf
-url="https://github.com/junegunn/fzf.git"\
-&& git clone --detph 1 "$url" ~/.fzf\
-&& ~/.fzf/install
-
-# googler
-url="https://raw.githubusercontent.com/jarun/googler/v4.3.1/googler"\
-&& sudo curl -o /usr/local/bin/googler "$url"\
-&& sudo chmod +x /usr/local/bin/googler
-
-# i3status
-url="https://i3wm.org/i3status/i3status-2.13.tar.bz2"\
-&& wget "$url"\
-&& tar -zxf i3status-*.tar.gz\
+(cd "$fi3status"\
 && autoreconf -fi\
 && mkdir build\
 && cd build\
 && ../configure --disable-sanitizers\
 && make -j$(nproc)\
 && sudo make install\
-&& cd ..
+&& cd ..)\
+|| echo "error i3status" >> "$ferror"
 
-# dmenu
-#url="https://dl.suckless.org/tools/dmenu-5.0.tar.gz"\
-#&& wget "$url"\
-#&& tar -zxf dmenu-*.tar.gz
+(mv "$ffzf" ~/.fzf\
+&& ~/.fzf/install)\
+|| echo "error googler" >> "$ferror"
 
-# st
-url="https://github.com/isbrqu/st/archive/master.zip"\
-&& wget "$url"\
-&& unzip -q st-master\
-&& cd st-master\
+(chmod +x googler\
+&& sudo mv googler /usr/local/bin/googler\
+&& sudo googler -u)\
+|| echo "error googler" >> "$ferror"
+
+(cd "$fst"\
 && sudo make clean install\
-&& cd ..
-
+&& cd ..)\
+|| echo "error st" >> "$ferror"
 
