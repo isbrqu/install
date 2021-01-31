facec507a7bdb7e42b00e333c1390269812900b7
Isaac Bruno Quiroga <isbrqu@gmail.com>
2021-01-31 19:51:55 -0300

    elimina startup

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-setxkbmap -layout us -variant altgr-intl
-xmodmap .config/dotfiles/keyboard-xmodmap/logitech-k400-fn-swap
-# xmodmap .config/dotfiles/keyboard-xmodmap/logitech-k400-dou
-xinput disable 10
-
