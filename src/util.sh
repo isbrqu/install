4db3b05dfe52006fe4ced1a0ccc2df00fc867860
Isaac Bruno Quiroga <isbrqu@gmail.com>
2021-11-26 02:38:39 -0300

    elimina impresiones en la terminal

--- src/util.sh
+++ src/util.sh
@@ -1,25 +1,23 @@
 clone() {
-    local url="$1"
-    local name="$2"
-    local version="$3"
-    local -a OPTS_GIT=(--depth 1 --silent)
+    local url="${1}"
+    local name="${2}"
+    local version="${3}"
+    local -a OPTS_GIT=(--quiet --depth 1)
     # local output="${FOLDER}/${name}"
-    if [[ "$version" == "-" ]];then
-        echo "with last version"
-        git clone --depth 1 -- "${url}" "$output"
-    else
-        git clone --depth 1 --branch="${version}" -- "${url}" "$output"
+    if [[ "${version}" != "-" ]];then
+        OPTS_GIT+=(--branch="${version}")
     fi
-    git clone 
+    git clone "${OPTS_GIT[@]}" -- "${url}" "${output}" 2> /dev/null
 }
 
 download() {
     local url="$1"
     local name="$2"
-    # local output="${FOLDER}/${name}"
+    # with wget
     # local -a OPTS_WGET=(--show-progress --continue)
-    local -a OPTS_WGET=(--quite --continue)
+    local -a OPTS_WGET=(--quiet --continue)
     wget "${OPTS_WGET[@]}" --output-document="${output}" "${url}"
+    # with curl
     # local -a OPTS_CURL=(--fail --silent --show-error --location)
     # curl "${OPTS_CURL[@]}" "${url}" --output "${output}"
 }
@@ -27,23 +25,24 @@ download() {
 update() {
     sudo apt update
     sudo apt upgrade -y
-    # xargs --arg-file="${REQUIREMENTS}" sudo apt install -y
 }
 
+# pkg() {
+#     echo "a"
+#     xargs --arg-file="${REQUIREMENTS}" sudo apt install -y
+# }
+
 download_programs() {
-    local list="$1"
-    local folder="$2"
+    local list="${1}"
+    local folder="${2}"
     local output
     {
     read _
     while read -r _ method name version url dest script type aux;do
-        if [[ "$type" = "cli"  || "$type" = "tui" ]];then
-            echo "method:   ${method}"
-            echo "name:     ${name}"
-            echo "version:  ${version}"
-            echo "url:      ${url}"
-            echo "dest:     ${dest}"
-            echo "aux:      ${aux}"
+        if [[ "${type}" = "cli"  || "${type}" = "tui" ]];then
+            echo "${name} ${version}"
+            # echo "dest:     ${dest}"
+            # echo "aux:      ${aux}"
             output="${folder}/${name}"
             case "${method}" in
                 download)
@@ -52,11 +51,11 @@ download_programs() {
                 clone)
                     clone "${url}" "${name}" "${version}" "${output}"
                 ;;
-                apt)
-                    echo "apt :D"
-                ;;
+                # apt)
+                #     # echo "apt :D"
+                # ;;
             esac
-            echo "${name} (${?})"
+            echo -e "error (${?})\n"
         fi
     done 
     } < "${list}"
